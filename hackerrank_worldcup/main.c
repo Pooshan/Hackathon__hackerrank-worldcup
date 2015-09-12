fddc7c14d4b25cf3493f917210b1bf0be46e3258
Pooshan <pooshan.vyas@gmail.com>
2015-09-12 13:17:23 -0700

    Initial Commit

---
+++ hackerrank_worldcup/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hackerrank_worldcup
+//
+//  Created by Pooshan Vyas on 9/12/15.
+//  Copyright © 2015 Pooshan Vyas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
